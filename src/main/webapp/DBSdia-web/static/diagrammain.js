5e9854bf854899345772800ab449df39690fac8a
heismelody <826556077@qq.com>
2016-10-06 00:16:17 +0800

    add: resize event

--- src/main/webapp/DBSdia-web/static/diagrammain.js
+++ src/main/webapp/DBSdia-web/static/diagrammain.js
@@ -1,5 +1,6 @@
 define(function(require, exports, module) {
-    var scrollBar = require('scrollBar');
+    var comp = require('components.js');
+    var vue = require('Vue');
 
     var init = function() {
       _initDom();
@@ -7,11 +8,64 @@ define(function(require, exports, module) {
     };
 
     function _initDom() {
+      (function initPanelHeight() {
+        let curClientWidth = comp.getClientHeight();
+
+        $('.design-panel').css('height',curClientWidth - 86);
+        $('.design-layout').css('height',curClientWidth - 86);
+      })();
+
 
     };
 
     function _initEvent() {
-      
+      (function initResizeEvent() {
+        //resize the panel height after event
+        $(window).on("resize", function(){
+          let curClientWidth = comp.getClientHeight();
+
+          if($('.row1').css('display') == "none") {
+            $('.design-panel').css('height',curClientWidth - 40);
+            $('.design-layout').css('height',curClientWidth - 40);
+          }
+          else {
+            $('.design-panel').css('height',curClientWidth - 86);
+            $('.design-layout').css('height',curClientWidth - 86);
+          }
+        });
+
+      })();
+
+      var vm = new Vue({
+        el: '#bar-collapse',
+        data: {
+          Uncollapsed:false,
+          collapsed:true,
+        },
+        methods: {
+          collapseHeaderEvent: function (event) {
+            let curClientWidth = comp.getClientHeight();
+
+            if($(".row1").css('display') == "none") {
+              $(".row1").slideDown(150);
+              this.Uncollapsed = false;
+              this.collapsed = true;
+
+              $('.design-panel').css('height',curClientWidth - 86);
+              $('.design-layout').css('height',curClientWidth - 86);
+            }
+            else {
+              $(".row1").slideUp(150);
+              this.Uncollapsed = true;
+              this.collapsed = false;
+
+              $('.design-panel').css('height',curClientWidth - 40);
+              $('.design-layout').css('height',curClientWidth - 40);
+            }
+          }
+        }
+      })
+
     };
 
     exports.init = init;
--- src/main/webapp/DBSdia-web/vendor/sea-modules/seajs.config.js
+++ src/main/webapp/DBSdia-web/vendor/sea-modules/seajs.config.js
@@ -1,6 +1,7 @@
 seajs.config({
   base: "../vendor/sea-modules/",
   alias: {
+    "bootstrap":"bootstrap/js/bootstrap.min.js",
     "scrollBar":"customScrollbar/jquery.mCustomScrollbar.concat.min.js",
     "Vue":"vue-1.0/vue.js",
   }
